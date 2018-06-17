ebee44e750bffab3b1a7b74b0e6a2ef3a58a253d
eobrien16 <37981262+eobrien16@users.noreply.github.com>
2018-06-17 22:11:52 +0100

    Add files via upload

---
+++ mod/invite.js
@@ -0,0 +1,8 @@
+exports.run = async (message, client, args) => {
+    await message.delete(0);
+    return message.channel.send({embed: {
+        color: 1851764,
+        title: "Invite this bot to other servers!",
+        description: "You can find this bots invite link [here](https://discordapp.com/oauth2/authorize?client_id=457996774808748043&permissions=8&scope=bot)"
+    }});
+}
