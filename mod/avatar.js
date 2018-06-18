48b7caa6249b91bf245ca5f9430724aec3cb296c
Unknown <eobrien16@ardscoil.ie>
2018-06-18 18:57:57 +0100

    make it whole

---
+++ mod/avatar.js
@@ -0,0 +1,21 @@
+exports.run = async (message, client, args) => {
+    await message.delete(0);
+    if (message.content.indexOf(' ') !== -1) {
+      let user = message.mentions.users.first();
+      let avtr = user.avatarURL;
+      return message.channel.send({embed: {
+        color: 1638655,
+        image: {
+          url: avtr
+        }
+      }})
+    } else {
+        let avtr = message.author.avatarURL;
+        return message.channel.send({embed: {
+          color: 1638655,
+          image: {
+            url: avtr
+          }
+        }})
+    }
+}
