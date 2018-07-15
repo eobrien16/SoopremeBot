a57b63b65c26e884e36f5289b8d8466613dae5a2
Unknown <eobrien16@ardscoil.ie>
2018-07-15 12:19:51 +0100

    Added purge command credit to faith

---
+++ mod/purge.js
@@ -0,0 +1,26 @@
+exports.run = async (message, client, args) => {
+    return message.delete(0);
+    const { text } = args;
+    const deleteCount = parseInt(args[0], 10);
+    if(!message.member.roles.some(r=>["Admins","Mods","Community Team"].includes(r.name)))
+        return message.channel.send({embed: {
+            color: 1241231,
+            title: "Fail!",
+            description: "You are not a high enough rank!"
+        }})
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.channel.send({embed: {
+            color: 882763,
+            title: "Fail!",
+            description: "Please select a number between 2 and 100"
+        }})
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    return message.channel.bulkDelete(fetched)
+        .catch(err => {
+            message.channel.send({embed: {
+                color: 87983,
+                title: "Fail!",
+                description: `Failed because of: ${err}`
+            }})
+        })
+}
